2bb54eb7f2295e544c11aefc6e13a11748e3f96a
Justin Silverman <jsilve24@gmail.com>
2025-03-25 14:47:00 -0400

    Revert "feat: speeding up dirichlet sampling by about 20%"
    This reverts commit 4edf2c15aa6dca52d160ac04760d5acbd837d77f.

--- R/dirichlet.R
+++ R/dirichlet.R
@@ -1,14 +1,16 @@
+
+
 ##' Function for sampling Dirichlet random variables
 ##'
 ##' @param n number of samples
-##' @param Alpha DxN matrix of Dirichlet parameters
+##' @param alpha D-vector of Dirichlet parameters
 ##' @return an D x n matrix of samples 
 ##' @author Justin Silverman
-rDirichletMat <- function(n, Alpha) {
-  D <- nrow(Alpha)
-  N <- ncol(Alpha)
-  logWpara <- array(rep(Alpha, times=n), dim=c(D, N, n))
-  logWpara[] <- rgamma(N*D*n, logWpara[], 1)
-  s <- colSums(logWpara, dims=1)
-  sweep(logWpara, c(2,3), s, FUN=`/`)
+rDirichlet <- function(n, alpha) {
+  D <- length(alpha)
+  out <- matrix(NA, D, n)
+  for (d in 1:D) {
+    out[d,] <- rgamma(n, alpha[d],1)
+  }
+  return(miniclo(out))
  }
