b04a170d3ea1918b49107d5610d12a1a18882ad9
kyle-mcgovern <kyle.mcgovern93@gmail.com>
2026-01-17 15:28:12 -0800

    docs for data

--- R/data.R
+++ R/data.R
@@ -1,21 +1,39 @@
-#' Gut Crohn's example dataset
+#' Gut Crohn's microbiome dataset (list)
 #'
-#' Example dataset used in vignettes and examples.
+#' This dataset contains a matrix and a data frame: genus-level microbiome profiles and
+#' corresponding sample metadata from a Crohn's disease case–control cohort.
+#' The dataset is used in examples and vignettes throughout the package.
 #'
-#' @format A data frame with <N> rows and <K> variables:
+#' @format A named list of one matrix and a dataframe:
 #' \describe{
-#'   \item{...}{...}
+#'   \item{\code{counts}}{A matrix with read counts with 195 rows and 95 columns)}
+#'   \item{\code{metadata}}{A data frame with 95 rows and 7 columns containing subject-level covariates.}
 #' }
-#' @source <where you got it / how generated>
+#'
+#' @details
+#' The \code{counts} matrix has one row per sample and one column per genus.
+#' The \code{metadata} data frame has one row per sample with metadata, critically Health.status either CD or Control, and Average cell count per gram frozen feces.
+#'
+#' @source Vandeputte, D. et al. Quantitative microbiome profiling links gut community variation to microbial load. Nature 551, 507–511 (2017).
 "gut_crohns_data"
 
-#' Oral mouthwash example dataset
+#' Gut Crohn's microbiome dataset (list)
 #'
-#' Example dataset used in vignettes and examples.
+#' This dataset contains a matrix and a data frame: genus-level microbiome profiles and
+#' corresponding sample metadata from an oral microbiome perturbation study.
+#' 28 participants' oral microbiomes were measured before, 15 minutes after, and 2 hours
+#' after perturbation with either a water control, antiseptic mouthwash, alchohol-free mouthwash,
+#' or soda. The dataset is used in examples and vignettes throughout the package.
 #'
-#' @format A data frame with <N> rows and <K> variables:
+#' @format A named list of one matrix and a dataframe:
 #' \describe{
-#'   \item{...}{...}
+#'   \item{\code{counts}}{A matrix with read counts with 116 rows and 81 columns)}
+#'   \item{\code{metadata}}{A data frame with 81 rows and 38 columns containing sample-level covariates.}
 #' }
-#' @source <where you got it / how generated>
+#'
+#' @details
+#' The \code{counts} matrix has one row per sample and one column per genus.
+#' The \code{metadata} data frame has one row per sample with metadata, critically the participant ID, flow cytometry average (and replicate) cells, time_c (time points), and treat (the perturbations )
+#'
+#' @source Marotz, C. et al. Quantifying live microbial load in human saliva samples over time reveals stable composition and dynamic load. mSystems 6 (2021).
 "oral_mouthwash_data"
